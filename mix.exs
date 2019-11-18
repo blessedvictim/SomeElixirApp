60b76c215a62e2298db6f4fd97dde7c247172242
BlessedVictim <tequila1000@yandex.ru>
2019-11-18 22:24:11 +0300

    small project cleanups

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Testapp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -34,10 +34,9 @@ defmodule Testapp.MixProject do
     [
       {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:redix, ">= 0.10.2"},
+      {:redix, ">= 0.10.2"}
     ]
   end
 end
